5e61fa634d461c1e0c7d5d404a3d6fb277a8f1dc
Andrey HP <andrey_ds@mail.ru>
2025-01-19 21:19:23 +0300

    upd

--- SnifferFilterAndDisplay/SnifferFilterAndDisplay.ino
+++ SnifferFilterAndDisplay/SnifferFilterAndDisplay.ino
@@ -41,12 +41,24 @@ void setup() {
         delay(100);
         goto START_INIT;
     }
-    CAN.init_Mask(1, 1, 0x1fffffff);
-    CAN.init_Filt(2, 1, canidVol);//volume from head unit
-//  CAN.init_Filt(3, 1, 0x17330b00);//cam display
+    
+    CAN.init_Mask(0, 1, 0x1fffffff);
+    CAN.init_Filt(0, 1, canidVol);//volume from head unit
+    CAN.init_Filt(1, 1, canidVol);//volume from head unit
+
+
+    CAN.init_Mask(1, 0, 0x07ff);
+    CAN.init_Filt(2, 0, canidWheelButton);
+    CAN.init_Filt(3, 0, canidWheelButton);
+
+/*
     CAN.init_Mask(0, 0, 0x07ff);
     CAN.init_Filt(0, 0, canidWheelButton);
 
+    CAN.init_Mask(1, 1, 0x1fffffff);
+    CAN.init_Filt(2, 1, canidVol);//volume from head unit
+//  CAN.init_Filt(3, 1, 0x17330b00);//cam display
+*/
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) { 
       Serial.println(F("oled fail")); //1160bytes for local variables is not enough for SSD to init. 1226-ok
 
@@ -68,15 +80,31 @@ void loop() {
     a++;
     CAN.readMsgBuf(&len, buf);
     canId = CAN.getCanId();
-    display.print(canId,HEX);
+    //display.print(canId,HEX);
+    if (canidVol == canId) {
+      display.print("v");
+    }
+    if (canidWheelButton == canId) {
+      display.print("w");
+    }
+    
       for (int i = 0; i<len; i++) {
         display.print(",");
         display.print(buf[i],HEX);
       }
-      display.println();
+
+      INT8U eflg = CAN.mcp2515_readRegister(MCP_EFLG);//rx1ovr,rx0ovr
+      //display.println();
+      display.print("+");
+      display.println(eflg,HEX);
+
+
+      display.display(); 
+ 
 	}
   if (a > 7) {
     a = 0;
+    display.setCursor(0, 0);
     display.clearDisplay();
     display.display(); 
   }
